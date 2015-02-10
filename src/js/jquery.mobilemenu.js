3e7c82ddf84aac1f33837c255e15b4fee64b5808
Valentín <htmgarcia@gmail.com>
2015-02-09 18:38:38 -0600

    Display heading and separators links in mobile

--- src/js/jquery.mobilemenu.js
+++ src/js/jquery.mobilemenu.js
@@ -1,78 +1,86 @@
 /**
- * jQuery Mobile Menu 
+ * jQuery Mobile Menu
  * Turn unordered list menu into dropdown select menu
  * version 1.1(27-JULY-2013)
- * 
+ *
  * Built on top of the jQuery library
  *   http://jquery.com
- * 
+ *
  * Documentation
  * 	 http://github.com/mambows/mobilemenu
+ * Notes
+ *   Slightly customised by OSTraining LLC
  */
 (function($){
-$.fn.mobileMenu = function(options) {
-	
-	var defaults = {
-			defaultText: 'Navigate to...',
-			className: 'select-menu',
-			subMenuClass: 'sub-menu',
-			subMenuDash: '&ndash;'
-		},
-		settings = $.extend( defaults, options ),
-		el = $(this);
-	
-	this.each(function(){
-		var $el = $(this),
-			$select_menu;
+	$.fn.mobileMenu = function(options) {
 
-		// ad class to submenu list
-		$el.find('ul').addClass(settings.subMenuClass);
+		var defaults = {
+				defaultText: 'Navigate to...',
+				className: 'select-menu',
+				subMenuClass: 'sub-menu',
+				subMenuDash: '&ndash;'
+			},
+			settings = $.extend( defaults, options ),
+			el = $(this);
 
-		// Create base menu
-		var $select_menu = $('<select />',{
-			'class' : settings.className + ' ' + el.get(0).className
-		}).insertAfter( $el );
+		this.each(function(){
+			var $el = $(this),
+				$select_menu;
 
-		// Create default option
-		$('<option />', {
-			"value"		: '#',
-			"text"		: settings.defaultText
-		}).appendTo( $select_menu );
+			// ad class to submenu list
+			$el.find('ul').addClass(settings.subMenuClass);
 
-		// Create select option from menu
-		$el.find('a').each(function(){
-			var $this 	= $(this),
-				optText	= '&nbsp;' + $this.text(),
-				optSub	= $this.parents( '.' + settings.subMenuClass ),
-				len		= optSub.length,
-				dash;
-			
-			// if menu has sub menu
-			if( $this.parents('ul').hasClass( settings.subMenuClass ) ) {
-				dash = Array( len+1 ).join( settings.subMenuDash );
-				optText = dash + optText;
-			}
+			// Create base menu
+			var $select_menu = $('<select />',{
+				'class' : settings.className + ' ' + el.get(0).className
+			}).insertAfter( $el );
 
-			// Now build menu and append it
+			// Create default option
 			$('<option />', {
-				"value"	: this.href,
-				"html"	: optText,
-				"selected" : (this.href == window.location.href)
+				"value"		: '#',
+				"text"		: settings.defaultText
 			}).appendTo( $select_menu );
 
-		}); // End el.find('a').each
+			/* Create select option from menu
+			 *  Added .separator, .nav-header selectors  */
+			$el.find('a, .separator, .nav-header').each(function(){
+				var $this 	= $(this),
+					optText	= '&nbsp;' + $this.text(),
+					optSub	= $this.parents( '.' + settings.subMenuClass ),
+					len		= optSub.length,
+					dash;
 
-		// Change event on select element
-		$select_menu.change(function(){
-			var locations = $(this).val();
-			if( locations !== '#' ) {
-				window.location.href = $(this).val();
-			};
-		});
+				// if menu has sub menu
+				if( $this.parents('ul').hasClass( settings.subMenuClass ) ) {
+					dash = Array( len+1 ).join( settings.subMenuDash );
+					optText = dash + optText;
+				}
 
-	}); // End this.each
+				// Set default href for .separator, .nav-header selectors
+				if( !this.href ){
+					this.href = "javascript:void(0)";
+				}
 
-	return this;
+				// Now build menu and append it
+				$('<option />', {
+					"value"	: this.href,
+					"html"	: optText,
+					"selected" : (this.href == window.location.href)
+				}).appendTo( $select_menu );
 
-};
+			}); // End el.find('a').each
+
+			// Change event on select element
+			$select_menu.change(function(){
+				var locations = $(this).val();
+				if( locations !== '#' ) {
+					window.location.href = $(this).val();
+				};
+			});
+
+		}); // End this.each
+
+		return this;
+
+	};
 })(jQuery);
