5200d38246cd277aee816e6ce3d5f00ad41cf19f
Valentín <htmgarcia@gmail.com>
2015-02-09 18:06:19 -0600

    Support main menu item type: separator

--- src/index.php
+++ src/index.php
@@ -136,13 +136,13 @@ JHtml::_('bootstrap.loadCss', false, $this->direction);
         #mainmenu > .active > a,
         #mainmenu > .active > a:hover,
         #mainmenu > .active > a:focus,
-        #mainmenu > .active > span.nav-header,
-        #mainmenu > .active > span.nav-header:hover,
-        #mainmenu > .active > span.nav-header:focus,
+        #mainmenu > .active > span,
+        #mainmenu > .active > span:hover,
+        #mainmenu > .active > span:focus,
         #mainmenu > li > a:hover,
         #mainmenu > li > a:focus,
-        #mainmenu > li > span.nav-header:hover,
-        #mainmenu > li > span.nav-header:focus {
+        #mainmenu > li > span:hover,
+        #mainmenu > li > span:focus {
             background-color: <?php echo $hover_color; ?>;
         }
     </style>
